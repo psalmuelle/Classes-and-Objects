c04c3949c0b272cde380000cf98718ca9a61bdcf
psalmuelle <bjoestores@gmail.com>
2022-05-27 13:13:08 +0100

    python task

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
+    def __init__(self,name:str, age:int, tracks:list, score:float):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
 
+    def change_name (self, new_name:str):
+        self.name = new_name
+    
+    def change_age (self, new_age:int):
+        self.age = new_age
 
+    def add_tracks (self, new_tracks):
+        self.tracks = new_tracks
+    
+    def get_score (self):
+        return self.score
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
